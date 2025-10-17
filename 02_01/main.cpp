91de9ca00a939919680963ffee7d4f92a57140a7
takahashi-yukito-0209 <k024g0011@g.neec.ac.jp>
2025-10-17 17:04:42 +0900

    02_01課題完了

---
+++ 02_01/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+
+
+template <typename T> 
+T Min(T a, T b)
+{
+    return (a < b) ? a : b;
+}
+
+
+int main()
+{
+    // int型の比較
+    int intA = 81;
+    int intB = 20;
+    std::cout << "int型: Min(" << intA << ", " << intB << ") = " << Min(intA, intB) << std::endl;
+
+    // float型の比較
+    float floatA = 2.5f;
+    float floatB = 0.2f;
+    std::cout << "float型: Min(" << floatA << ", " << floatB << ") = " << Min(floatA, floatB) << std::endl;
+
+    // double型の比較
+    double doubleA = 0.29;
+    double doubleB = 2.09;
+    std::cout << "double型: Min(" << doubleA << ", " << doubleB << ") = " << Min(doubleA, doubleB) << std::endl;
+
+    return 0;
+}
